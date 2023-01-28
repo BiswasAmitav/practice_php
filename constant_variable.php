f9f32abf5bc4e956b4d1e7067dd3a78c760546a1
BiswasAmitav <biswasamit2648@gmail.com>
2023-01-28 14:18:53 +0530

    here i learnt all about constant varible

---
+++ constant_variable.php
@@ -0,0 +1,13 @@
+<?php
+// STRUCTURE OF CONSTANT VARIABLE .
+
+//   define(num,500, true )....like below  fi we write true here ...case sensitive will be removed ....
+
+
+
+    define("test",5000);
+    //define("test",50);  can't be duplicated ....
+
+
+    echo test;
+?>
