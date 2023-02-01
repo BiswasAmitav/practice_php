72f4cece0ea722c00ae72f699109a659b007eed7
BiswasAmitav <biswasamit2648@gmail.com>
2023-02-01 13:18:16 +0530

    all about funciton

---
+++ function.php
@@ -0,0 +1,27 @@
+<?php
+function myName() {
+    echo "Amitav Biswas";
+}
+
+myName();
+
+
+
+
+
+// Function with parameters ..
+function sum($a,$b){
+    echo $a+$b;
+}
+
+sum(10,20);
+
+
+
+// Greeting message ....
+function greting($n){
+    echo "Hello ". $n;
+}
+
+greting("Amitav");
+?>
