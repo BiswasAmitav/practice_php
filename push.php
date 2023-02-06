beb3cbe215e8a99ed0c676486895954634947dae
BiswasAmitav <biswasamit2648@gmail.com>
2023-02-06 20:55:16 +0530

    in this tutorial i learnt how to push or pop element from the array

---
+++ push.php
@@ -0,0 +1,12 @@
+<?php
+ $fruits = ["Orange", "Banana", "apple","grapes"];
+
+ array_pop($fruits);// delete the elements 
+ print_r($fruits); // 
+
+
+
+ array_push($fruits, "Jachfruits");
+
+ print_r($fruits); 
+?>
