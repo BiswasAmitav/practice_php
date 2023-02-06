d5b3b09308a1f8405448f37f5b48365c5fd1da4f
BiswasAmitav <biswasamit2648@gmail.com>
2023-02-06 21:07:14 +0530

    In this lesson i learnt how to add and remove element from the beginning

---
+++ shift_unshift.php
@@ -0,0 +1,14 @@
+<?php
+$colors =[" orange", " green", " blue"];
+
+
+array_shift($colors);// Its remove element from the beginning position 
+
+print_r($colors);
+
+
+array_unshift($colors, "pink","grey","black");// ItS add element from the beginning 
+
+print_r($colors);
+
+?>
