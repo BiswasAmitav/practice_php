b775dce38742f39ee847c54132a3d95bd36845f5
BiswasAmitav <biswasamit2648@gmail.com>
2023-02-08 21:19:04 +0530

    In this tutorial i learnt how to deal with the key funciton

---
+++ key_function.php
@@ -0,0 +1,13 @@
+<?php
+
+$color = ["one"=>"red","Two"=>'green',"Three"=>"blue","Four"=>"yellow"];
+
+$newArray=array_keys($color);
+$newArray1=array_key_first($newArray);
+
+//print_r($newArray);
+print_r($newArray1);
+
+
+
+?>
