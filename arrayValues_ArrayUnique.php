be68f7370e75bc372f44e54a9e70c16b29c14a8b
BiswasAmitav <biswasamit2648@gmail.com>
2023-02-14 20:53:51 +0530

    By this code i learnt hw to remove the duplicates element into array and returns that array as a unique

---
+++ arrayValues_ArrayUnique.php
@@ -0,0 +1,11 @@
+<?php
+
+$a = ["a"=>"Sanjay","b"=>"Aman","c"=>"Mohan", "d"=>"Mohan"];
+
+
+$new_array=array_unique($a);// It removes the duplicates value form the and returns unique array ></div>
+
+
+
+print_r($new_array);
+?>
