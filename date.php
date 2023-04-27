baa283159d0e49acd6ba46788f5811601222bda8
BiswasAmitav <biswasamit2648@gmail.com>
2023-04-27 22:11:58 +0530

    in this lesson i practiced how to deal with the date funtion

---
+++ date.php
@@ -0,0 +1,10 @@
+<?php
+
+    echo "Hour is ". date("h") . "<br>";
+    echo "Minutes is " . date("i") . "<br>";
+    echo "Seconds is " . date("s") . "<br>";
+    echo "Meridiem is " . date("A") . "<br>";
+// here is the code for showing all the information together ...
+echo " Time is " . date("h:i:s a") . "<br>";
+
+?>
