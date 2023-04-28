2f1306063930ebec0539c52576e3c13c5b207299
BiswasAmitav <biswasamit2648@gmail.com>
2023-04-28 10:33:09 +0530

    Trim funciton is used to remove cahr from both side either end or starting

---
+++ Trim.php
@@ -0,0 +1,18 @@
+<?php
+    $str = "Yahoo Baba";
+
+    echo $str . "<br>";
+
+    echo trim($str, "aY"); //It checks only starting position or ending position ...
+    // Suppose if I write b it will not return anything because the text is not either stated or ended with b .
+
+
+//chop()-> works as exactly right trim .
+// rtrim()-> remove from the right side ...
+//ltrim()->remove from the left side .. 
+
+
+
+
+
+?>
