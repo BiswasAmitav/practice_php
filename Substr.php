f34af1e4036942adba694e7671a3b9c8e49bbe28
BiswasAmitav <biswasamit2648@gmail.com>
2023-04-28 10:43:26 +0530

    Here i learnt thehow to make the sub str

---
+++ Substr.php
@@ -0,0 +1,9 @@
+<?php
+     
+     $str = "Helo world. The world is nice";
+
+     echo substr($str,5) // here i did not mentioned the ending position 
+    // It removes the Helo after the that till end it will be printed " 
+
+    
+?>
